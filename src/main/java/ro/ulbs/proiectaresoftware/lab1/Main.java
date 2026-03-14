004b608892d1b55d4627f7d30b6c9f3f9576bc99
RaulCaliman <211803244+Darian-Raul-Caliman@users.noreply.github.com>
2026-03-14 13:48:20 +0200

    Laborator1

--- src/main/java/ro/ulbs/proiectaresoftware/lab1/Main.java
+++ src/main/java/ro/ulbs/proiectaresoftware/lab1/Main.java
@@ -1,15 +1,38 @@
 package ro.ulbs.proiectaresoftware.lab1;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        String alfabet = new String();
-        for(char c = 'a' ; c<= 'z' ; c++){
-            alfabet =  alfabet + c;
-        }
-        alfabet = alfabet.toLowerCase();
-        System.out.println(alfabet);
-        alfabet = alfabet.toUpperCase();
-        System.out.println(alfabet);
+        String alfabet = "";
+        ArrayList<String> listaGrupuri = new ArrayList<String>();
+        String vocale = "aeiou";
 
+        for(char c = 'a' ; c <= 'z' ; c++){
+            alfabet = alfabet + c;
+        }
+
+        System.out.println(alfabet.toLowerCase());
+        System.out.println(alfabet.toUpperCase());
+
+
+        alfabet = alfabet.toLowerCase();
+
+        String grupCurent = "";
+
+        for(int i = 0; i < alfabet.length(); i++){
+
+            grupCurent = grupCurent + alfabet.substring(i, i+1);
+            if (i < alfabet.length() - 1) {
+
+                if (vocale.contains(alfabet.substring(i+1, i+2))) {
+                    listaGrupuri.add(grupCurent);
+                    grupCurent = "";
+                }
+            }
+        }
+
+        listaGrupuri.add(grupCurent);
+        System.out.println(listaGrupuri);
     }
 }
