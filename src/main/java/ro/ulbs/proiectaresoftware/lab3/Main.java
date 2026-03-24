5bb64e8be626aeef5a6d79fa8dd60992acdd84c2
RaulCaliman <211803244+Darian-Raul-Caliman@users.noreply.github.com>
2026-03-24 21:52:42 +0200

    Laborator 3 A)

---
+++ src/main/java/ro/ulbs/proiectaresoftware/lab3/Main.java
@@ -0,0 +1,48 @@
+package ro.ulbs.proiectaresoftware.lab3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static java.lang.System.in;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String fileName = "C:\\Users\\raulc\\IdeaProjects\\LaboratoareSoftware\\src\\main\\java\\ro\\ulbs\\proiectaresoftware\\lab3\\text.txt";
+        Path path = Paths.get(fileName);
+        List<String> text = citireFisier(path);
+        modificari(text);
+
+
+
+    }
+
+    public static void modificari(List<String> ListaNoua) {
+
+        for (int i = 0; i < ListaNoua.size(); i++) {
+            String liniaCurenta = ListaNoua.get(i);
+            ListaNoua.set(i, liniaCurenta + "\n");
+        }
+        System.out.println("Textul rezultat:");
+        for (String linie : ListaNoua) {
+            System.out.print(linie);
+        }
+    }
+
+    public static List<String> citireFisier(Path path) {
+        List<String> lines = new ArrayList<>();
+
+        try {
+
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            System.out.println("Eroare la citire: " + e.getMessage());
+        }
+
+
+        return lines;
+    }
+}
